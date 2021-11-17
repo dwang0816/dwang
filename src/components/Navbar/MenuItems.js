9db62e2cba74cea4ea1cb445a300da620a8277dc
Dennis Wang <@dwang0816@gmail.com>
2021-11-17 15:48:30 -0500

    finished setting up navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,18 @@
+export const MenuItems = [
+  {
+    title: "Linkedin",
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    title: "Medium",
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    title: "Github",
+    url: '#',
+    cName: 'nav-links'
+  }
+
+]
