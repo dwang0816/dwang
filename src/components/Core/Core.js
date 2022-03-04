55b8be0fc81fb084d42ad11e12f05a54036cc029
Dennis Wang <dwang0816@gmail.com>
2022-03-03 22:35:52 -0500

    new project

--- src/components/Core/Core.js
+++ src/components/Core/Core.js
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import React, { Component } from 'react'
 import Project1 from './Project1'
 import Project2 from './Project2'
+import Project3 from './Project3'
 import "./Core.css"
 
 export default class Core extends Component {
@@ -11,6 +12,7 @@ export default class Core extends Component {
     console.log(THREE)
     return (
       <div className="core-container">
+        <Project3/>
         <Project1/>
         <Project2/>
         
---
+++ src/components/Core/Project3.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Project3.css'
+import moodiday from '../../images/moodiday.gif'
+
+const Project3 = () => {
+  return (
+    <div>
+      <section>
+        <div className="project-container">
+          <img className="project-picture" alt="moodiday" src={moodiday}/>
+          <h1 className="project-title">Moodiday</h1>
+          <p className="project-description">Find New Ways to Chill Out.</p>
+          <hr className="break"/>
+          <p className="project-stacks">Next.js, Scss, Netlify.</p>
+          <div className="project-links">
+          <a href="https://moodiday.com/" className="project-site">Site</a>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+export default Project3
