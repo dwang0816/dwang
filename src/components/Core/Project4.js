1f2ef908c206c1fa15179cd344a930642773c333
Dennis Wang <48970587+dwang0816@users.noreply.github.com>
2025-03-09 21:10:04 -0400

    Update Project4.js

--- src/components/Core/Project4.js
+++ src/components/Core/Project4.js
@@ -9,7 +9,7 @@ const Project4 = () => {
         <div className="project-container">
           <img className="project-picture" alt="iwomantv" src={iwomantv}/>
           <h1 className="project-title">iWomanTV</h1>
-          <p className="project-description">A social media platform focused in engagement with women and minorities.</p>
+          <p className="project-description">A social media platform dedicated to fostering meaningful engagement and empowerment for women and minorities. ✨🌍 Built to amplify diverse voices, encourage open conversations, and create an inclusive space where everyone feels heard and valued. Whether through discussions, networking, or community-driven initiatives, this platform is designed to inspire connection and change. 💬🤝</p>
           <hr className="break"/>
           <p className="project-stacks">Wordpress, PHP.</p>
           <div className="project-links">
