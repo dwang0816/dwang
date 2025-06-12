debf9deb405a1f9d90960c49b323799e0b1389e8
dwang0816 <dwang0816@gmail.com>
2025-06-11 22:17:00 -0400

    added new colors

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -52,7 +52,7 @@ const Highlights = () => {
             </div>
             <div className="highlight-links">
               <a 
-                href="https://app.netlify.com/projects/mental-gym/deploys/6849fe06728fba5e015ce345" 
+                href="https://6849fe06728fba5e015ce345--mental-gym.netlify.app" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="primary-link"
